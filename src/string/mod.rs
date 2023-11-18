8f2949d217a8fd71b6a4d3db3560a915b7b42161
Blue_Sugar <aoicchi0620@gmail.com>
2023-11-19 04:33:12 +0900

    add: run length

--- src/string/mod.rs
+++ src/string/mod.rs
@@ -5,3 +5,14 @@ fn is_palindrome(s: Vec<u8>) -> bool {
     s == t
 }
 
+#[allow(unused)]
+fn run_length_encoding<T: Eq>(a: Vec<T>) -> Vec<(T, usize)> {
+    let mut a = a.into_iter().map(|a| (a, 1)).collect::<Vec<_>>();
+    a.dedup_by(|a, b| {
+        a.0 == b.0 && {
+            b.1 += a.1;
+            true
+        }
+    });
+    a
+}
