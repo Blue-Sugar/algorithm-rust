8795dd7f21037b61e62073cc427f3d016a901c0a
Blue_Sugar <aoicchi0620@gmail.com>
2023-11-19 20:36:59 +0900

    add: ax + by = d function

--- src/math/gcd.rs
+++ src/math/gcd.rs
@@ -16,3 +16,19 @@ fn lcm(a: usize, b: usize) -> usize {
 fn is_coprime(a: usize, b: usize) -> bool {
     gcd(a, b) == 1
 }
+
+#[allow(unused)]
+// O(log(max{a, b}))
+// ax + by = d (d = gcd(a, b)), so return is (x, y)
+fn axby(mut a: usize, mut b: usize) -> (isize, isize) {
+    let mut history = vec![];
+    while b > 0 {
+        history.push((a, b));
+        (a, b) = (b, a % b);
+    }
+    let (mut x, mut y) = (1, 0);
+    for &(a, b) in history.iter().rev() {
+        (x, y) = (y, x - a as isize / b as isize * y);
+    }
+    (x, y)
+}
