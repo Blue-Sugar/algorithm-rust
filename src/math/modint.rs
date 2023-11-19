5ab24d315658867f082aee2c4b3cef6d7369ee24
Blue_Sugar <aoicchi0620@gmail.com>
2023-11-19 19:49:36 +0900

    add: dobling pow

--- src/math/modint.rs
+++ src/math/modint.rs
@@ -11,6 +11,21 @@ impl<const M: usize> ModUsize<M> {
     pub fn new(n: usize) -> Self {
         ModUsize { value: n % M }
     }
+
+    // a^n (mod m)
+    // O(log n)
+    pub fn pow(&self, mut n: usize) -> ModUsize<M> {
+        let mut res = ModUsize::<M>::new(1);
+        let mut a = self.clone();
+        while n > 0 {
+            if n % 2 == 1 {
+                res = res * a;
+            }
+            a = a * a;
+            n /= 2;
+        }
+        res
+    }   
 }
 
 impl<const M: usize> Add for ModUsize<M> {
