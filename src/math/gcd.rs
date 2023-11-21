69c85626ff7b78ef4261e102a4046388448356fc
Blue_Sugar <aoicchi0620@gmail.com>
2023-11-21 10:19:20 +0900

    add: chinese remainder theorem

--- src/math/gcd.rs
+++ src/math/gcd.rs
@@ -32,3 +32,23 @@ fn axby(mut a: usize, mut b: usize) -> (isize, isize) {
     }
     (x, y)
 }
+
+#[allow(unused)]
+// a = a0 (mod m0), a = a1 (mod m1) <=> a = return + m0m1 k (k \in Z)
+// return is the most smallest in natural.
+// n0, m1: should be coprime.
+fn chinese_remainder_theorem(
+    a0: usize, 
+    m0: usize,
+    a1: usize,
+    m1: usize,
+) -> usize {
+    let (x0, x1) = axby(m0, m1);
+    let x = a0 as isize * m1 as isize * x1 + a1 as isize * m0 as isize * x0;
+    let mut res = x % (m0 * m1) as isize;
+    if res < 0 {
+        res += (m0 * m1) as isize;
+    }
+    res as usize
+}
+
